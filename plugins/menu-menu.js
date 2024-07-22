71ec4b43904383f206ff73fd33262e0567b54a14
AdrianOficial <120221652+The-DiosBot-MD@users.noreply.github.com>
2024-07-22 15:51:41 -0400

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -607,7 +607,7 @@ console.log(e)}
 }
 
 //handler.command = /^(menu|menú|memu|memú|help|info|comandos|2help|menu1.2|ayuda|commands|commandos|menucompleto|allmenu|allm|m|\?)$/i
-handler.command = /^(menucompleto|allmenu|\?)$/i
+handler.command = /^(menucompleto|allmenu|menu|\?)$/i
 handler.register = true
 export default handler
 
